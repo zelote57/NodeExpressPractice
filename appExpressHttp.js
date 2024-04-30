e69e0eed495df6eb1a060414e22d7492ac3cdd2e
Gustavo Lozada <zelote57@gmail.com>
2024-04-29 20:37:53 -0500

    add express

--- appExpressHttp.js
+++ appExpressHttp.js
@@ -3,7 +3,36 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next)=>{
-    res.send('<h1>Hola Estudiantes!</h1>');
+    let body = '';
+    
+    req.on('end', ()=>{
+        const userName = body.split('=')[1]
+        if(userName){
+            console.log(userName);
+            req.body = {name : userName};
+        }
+        next();
+    })
+
+    req.on('data', (chunk)=>{
+        body += chunk;
+    });    
+});
+
+app.use((req, res, next)=>{
+    
+    if (req.body){
+        res.send('User: ' + req.body.name);
+    }
+    else{
+        res.send(`<form method="POST">
+         <input type="text" name="user-name"/>
+         <button>Create User</button>
+         </form>`);
+    }
+    
+    
+
 });
 
 app.listen(5001);
